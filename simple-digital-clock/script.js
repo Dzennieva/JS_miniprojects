73c36311785e9b06aa42c583049f7cb41a4249ce
Dzennieva <jenniferajibo@gmail.com>
2022-12-26 08:50:36 +0100

    added a colon to the timer

--- simple-digital-clock/script.js
+++ simple-digital-clock/script.js
@@ -1,28 +1,28 @@
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('mins');
-const seconds = document.getElementById('secs');
-
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("mins");
+const seconds = document.getElementById("secs");
 
 function getTime() {
-    const currentDay = new Date();
-    let h = currentDay.getHours();
-    let m = currentDay.getMinutes();
-    let s = currentDay.getSeconds();
+  const currentDay = new Date();
+  let h = currentDay.getHours();
+  let m = currentDay.getMinutes();
+  let s = currentDay.getSeconds();
 
-    h = addZero(h);
-    m = addZero(m);
-    s = addZero(s);
+  h = addZero(h);
+  m = addZero(m);
+  s = addZero(s);
 
-    hours.innerHTML = h;
-    minutes.innerHTML = m;
-    seconds.innerHTML = s;
-    setTimeout(getTime, 1000);
+  hours.innerHTML = h + " :";
+  minutes.innerHTML = m + " :";
+  seconds.innerHTML = s + ":";
+
+  setTimeout(getTime, 1000);
 }
-
 getTime();
 
 function addZero(i) {
-    if(i < 10) {i = '0' + i};
-    return i;
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
-
